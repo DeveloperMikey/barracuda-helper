8dc111d23189a991cf0668c6c649046badd916a9
DeveloperMikey <maemeesmihkel@gmail.com>
2025-11-23 17:52:36 +0200

    Fix ConfigGroup

--- src/main/java/dev/mikeya/barracudahelper/BarracudaHelperConfig.java
+++ src/main/java/dev/mikeya/barracudahelper/BarracudaHelperConfig.java
@@ -2,10 +2,8 @@ package dev.mikeya.barracudahelper;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
-import net.runelite.client.config.ConfigSection;
 
-@ConfigGroup("example")
+@ConfigGroup("barracuda-helper")
 public interface BarracudaHelperConfig extends Config
 {
 
